c24a5176de37afb39a9b15219cf7b7371c4bf43a
LKH <gorloom6425@naver.com>
2020-07-17 01:05:13 +0900

    함수

--- study.js
+++ study.js
@@ -1,53 +1,14 @@
-var empty_object ={};
-var stooge = {
-    "first_name": "Lim",
-    "last_name": "keunhyeok"
-};
+var fibonacci = function () {
+    var memo = [0, 1];
+    var fib = function (n) {
+        var result = memo[n];
+        if (typeof result !== 'number') {
+            result = fib(n - 1) + fib(n - 2);
+            memo[n] = result;
+        }
+        return result;
+    };
+    return fib;
+}();
 
-console.log(stooge.first_name);
-console.log(stooge["last_name"]);
-console.log(stooge["middle_name"]);
-
-stooge.last_name = 'KH';
-stooge.nickname = 'Curly';
-
-console.log(stooge.last_name);
-console.log(stooge.nickname);
-
-var name;
-
-for (name in stooge) {
-    if (typeof stooge[name] !== 'function') {
-        console.log(name + ': ' + stooge[name]);
-    }
-}
-
-var another_stooge = Object.create(stooge);
-another_stooge['first_name'] = 'Harry';
-another_stooge['nickname'] = 'Moe';
-
-console.log(another_stooge.nickname);
-delete another_stooge.nickname;
-console.log(another_stooge.nickname);
-
-var MYAPP = {};
-
-MYAPP.stooge = {
-    "first_name": "Joe",
-    "last_name": "Howard"
-};
-
-MYAPP.flight = {
-    airline: "Oceanic",
-    number: 815,
-    departure: {
-        IATA: "SYD",
-        time: "2020-09-22 14:55",
-        city: "Sydeny"
-    },
-    arrival: {
-        IATA: "LAX",
-        time: "2020-09-23 10:42",
-        city: "Los Angeles"
-    }
-};
+console.log(fibonacci(10));
