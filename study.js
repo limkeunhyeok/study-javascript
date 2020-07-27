9ca089e3f2470109c89e591bac0f5be841c39c88
LKH <gorloom6425@naver.com>
2020-07-27 19:42:28 +0900

    nodejs

--- study.js
+++ study.js
@@ -1,41 +1,18 @@
-// define the Person Class
-function Person() {}
+function first () {
+    second();
+    console.log('첫 번째');
+}
 
-Person.prototype.walk = function () {
-    console.log('I am walking');
+function second () {
+    third();
+    console.log('두 번째');
+}
+
+function third () {
+    console.log('세 번째');
 };
 
-Person.prototype.sayHello = function () {
-    console.log('hello');
-};
-
-// define the Student Class
-function Student () {
-    // Call the parent constructor
-    Person.call(this);
-};
-
-// inherit Person
-Student.prototype = new Person();
-
-// correct the constructor pointer because it points to Person
-Student.prototype.construcrtor = Student;
-
-// replace the sayHello method
-Student.prototype.sayHello = function () {
-    console.log('hi, i am a student');
-};
-
-// add sayGoodbye method
-Student.prototype.sayGoodbye = function () {
-    console.log('goodbye');
-};
-
-var student1 = new Student();
-student1.sayHello(); // hi, i am a student
-student1.walk(); // I am walking
-student1.sayGoodbye(); // goodbye
-
-// check inheritance
-console.log(student1 instanceof Person); // true
-console.log(student1 instanceof Student); // true
+first();
+// 세 번째
+// 두 번째
+// 첫 번째
