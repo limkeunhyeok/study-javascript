abfccd8b20e44a095c57bd56d1586aebf5defed6
LKH <gorloom6425@naver.com>
2020-07-30 15:23:30 +0900

    타입

--- study.js
+++ study.js
@@ -1,18 +1,3 @@
-function first () {
-    second();
-    console.log('첫 번째');
-}
-
-function second () {
-    third();
-    console.log('두 번째');
-}
-
-function third () {
-    console.log('세 번째');
-};
-
-first();
-// 세 번째
-// 두 번째
-// 첫 번째
+var a = [];
+a["13"] = 42;
+console.log(a.length); // 14
