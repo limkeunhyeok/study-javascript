6457cf59cef4602c423e5ef5224fb98b66e1dcce
LKH <gorloom6425@naver.com>
2020-08-09 00:55:28 +0900

    문법 (2)

--- study.js
+++ study.js
@@ -1,19 +1,37 @@
-foo: for (var i = 0; i < 4; i++) {
-    for (var j = 0; j < 4; j++) {
-        if ((i * j) >= 3) {
-            console.log('그만!', i, j);
-            break foo;
-        }
+function foo() {
+    try {
+        return 42;
+    }
+    finally {
+        console.log('hello');
+    }
+    console.log('실행되지 않는다');
+}
 
-        console.log(i, j);
+console.log(foo());
+// hello
+// 42
+
+for (var i = 0; i < 10; i++) {
+    try {
+        // console.log(i)
+        continue;
+    }
+    finally {
+        console.log(i);
     }
 }
 
-// 0 0
-// 0 1
-// 0 2
-// 0 3
-// 1 0
-// 1 1
-// 1 2
-// 그만! 1 3
+var a = '42';
+
+switch (true) {
+    case a == 10:
+        console.log("10 또는 '10'");
+        break;
+    case a == 42:
+        console.log("42 또는 '42'");
+        break;
+    default:
+        // 여기엔 오지 않는다.
+}
+// 42 또는 '42'
