8d8a405d9d4cc32c13badbe78238a44e8614dfc3
LKH <gorloom6425@naver.com>
2020-08-26 03:55:02 +0900

    프로토타입 (1)

--- study.js
+++ study.js
@@ -1,12 +1,26 @@
-var anotherObject = {
-    a: 2
-};
-
-// anotherObject에 연결된 객체를 생성한다
-var myObject = Object.create(anotherObject);
-
-for (var k in myObject) {
-    console.log(k + "를 발견!");
+function Foo(name) {
+    this.name = name;
 }
 
-console.log("a" in myObject); // true
+Foo.prototype.myName = function() {
+    return this.name;
+};
+
+function Bar(name, label) {
+    Foo.call(this, name);
+    this.label = label;
+}
+
+// Bar.prototype를 Foo.prototype에 연결한다
+Bar.prototype = Object.create(Foo.prototype);
+
+// 여기서 Bar.prototype.constructor은 사라졌으니
+// 이 프로퍼티에 의존하는 코드가 있다면 수동으로 일일이 해결해야 한다
+Bar.prototype.myLabel = function() {
+    return this.label;
+};
+
+var a = new Bar("a", "obj a");
+
+console.log(a.myName());
+console.log(a.myLabel());
