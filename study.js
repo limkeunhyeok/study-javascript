3535d7e5e0c85f1d9beba79ef75c8688ab2ce020
LKH <gorloom6425@naver.com>
2020-08-31 21:45:35 +0900

    작동 위임 (2)

--- study.js
+++ study.js
@@ -1,22 +1,58 @@
-Foo = {
-    init: function(who) {
-        this.me = who;
+var LoginController = {
+    errors = [],
+    getUser: function() {
+        return document.getElementById("login_username").value;
     },
-    identify: function() {
-        return "I am " + this.me;
+    getPassword: function() {
+        return document.getElementById("login_password").value;
+    },
+    validateEntry: function(user, pw) {
+        user = user || this.getUser();
+        pw = pw || this.getPassword();
+    
+        if (!(user && pw)) {
+            return this.failure("ID와 비밀번호를 입력하여 주십시오!");
+        } else if (user.length < 5) {
+            return this.failure("비밀번호는 5자 이상이어야 합니다.!");
+        }
+    
+        return true;
+    },
+    showDialog: function(title, msg) {
+        // 사용자에게 다이얼로그 창으로 타이틀과 메시지를 표시한다
+    },
+    failure: function(err) {
+        this.errors.push(err);
+        this.showDialog("에러", "로그인 실패: " + err);
     }
 };
 
-Bar = Object.create(Foo);
+// AuthController가 LoginController에 위임하도록 연결한다
+var AuthController = Object.create(LoginController);
+    
+AuthController.errors = [];
+AuthController.checkAuth = function() {
+    var user = this.login.getUser();
+    var pw = this.login.getPassword();
 
-Bar.speak = function() {
-    console.log("Hello, " + this.identify() + ".");
+    if (this.login.validateEntry(user, pw)) {
+        this.server("/check-auth", {
+            user: user,
+            pw:pw
+        })
+        .then(this.success.bind(this))
+        .fail(this.failure.bind(this));
+    }
 };
-
-var b1 = Object.create(Bar);
-b1.init("b1");
-var b2 = Object.create(Bar);
-b2.init("b2");
-
-b1.speak();
-b2.speak();
+AuthController.server = function(url, data) {
+    return $.ajax({
+        url: url,
+        data: data
+    });
+};
+AuthController.accepted = function() {
+    this.showDialog("성공", "인증 성공!");
+};
+AuthController.rejected = function(err) {
+    this.failure("인증 실패: " + err);
+};
