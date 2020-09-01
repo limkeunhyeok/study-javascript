5d0eefa2384f0bde937a5287364148da72350dfb
LKH <gorloom6425@naver.com>
2020-09-02 00:22:17 +0900

    비동기성_지금과 나중 (1)

--- study.js
+++ study.js
@@ -1,58 +1,11 @@
-var LoginController = {
-    errors = [],
-    getUser: function() {
-        return document.getElementById("login_username").value;
-    },
-    getPassword: function() {
-        return document.getElementById("login_password").value;
-    },
-    validateEntry: function(user, pw) {
-        user = user || this.getUser();
-        pw = pw || this.getPassword();
-    
-        if (!(user && pw)) {
-            return this.failure("ID와 비밀번호를 입력하여 주십시오!");
-        } else if (user.length < 5) {
-            return this.failure("비밀번호는 5자 이상이어야 합니다.!");
-        }
-    
-        return true;
-    },
-    showDialog: function(title, msg) {
-        // 사용자에게 다이얼로그 창으로 타이틀과 메시지를 표시한다
-    },
-    failure: function(err) {
-        this.errors.push(err);
-        this.showDialog("에러", "로그인 실패: " + err);
-    }
-};
+function now() {
+    return 21;
+}
 
-// AuthController가 LoginController에 위임하도록 연결한다
-var AuthController = Object.create(LoginController);
-    
-AuthController.errors = [];
-AuthController.checkAuth = function() {
-    var user = this.login.getUser();
-    var pw = this.login.getPassword();
+function later() {
+    answer = answer * 2;
+    console.log("answer:", answer);
+}
 
-    if (this.login.validateEntry(user, pw)) {
-        this.server("/check-auth", {
-            user: user,
-            pw:pw
-        })
-        .then(this.success.bind(this))
-        .fail(this.failure.bind(this));
-    }
-};
-AuthController.server = function(url, data) {
-    return $.ajax({
-        url: url,
-        data: data
-    });
-};
-AuthController.accepted = function() {
-    this.showDialog("성공", "인증 성공!");
-};
-AuthController.rejected = function(err) {
-    this.failure("인증 실패: " + err);
-};
+var answer = now();
+setTimeout(later, 1000); // answer: 42
