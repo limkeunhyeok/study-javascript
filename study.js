c1cc14d2e0dccf56bc89d71ce2e1febe69a93dd3
LKH <gorloom6425@naver.com>
2020-09-19 03:25:12 +0900

    제너레이터 (3)

--- study.js
+++ study.js
@@ -1,17 +1,25 @@
-function *main() {
-    var x = yield "hello world!";
-    
-    // 실행되지 않음
-    console.log(x);
+// 제너레이터 아닌 일반 함수다.
+function bar(url1, url2) {
+    return Promise.all([
+        request(url1),
+        request(url2)
+    ]);
 }
 
-var it = main();
-it.next();
+function *foo() {
+    // 프로미스형 동시성 관련 세부분은 감춘다.
+    // bar() 내부
+    var results = yield bar(
+        "http://some.url.1",
+        "http://some.url.2"
+    );
 
-try {
-    it.throw("???");
-} catch (error) {
-    console.log(error);
+    var r1 = results[0];
+    var r2 = results[1];
+
+    var r3 = yield request("http://some.url.3/?v=" + r1 + "," + r2);
+
+    console.log(r3);
 }
-
-// ???
+// 앞서 정의한 run 유틸리티
+run(foo);
