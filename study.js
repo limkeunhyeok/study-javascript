5bab9690e4ca68c2ea9e6eeeabeb7d7106467ff2
LKH <gorloom6425@naver.com>
2020-09-26 01:31:35 +0900

    벤치마킹과 튜닝 (1)

--- study.js
+++ study.js
@@ -1,63 +1,17 @@
-function foo(url) {
-    var state; // 제너레이터 상태 관리
-    var val; // 제너레이터 스코프 변수 선언
+// 케이스 1
+var x = [];
+for (var i = 0; i < 10; i++) {
+    x[i] = "x";
+}
 
-    function process(v) {
-        switch (state) {
-            case 1:
-                console.log("요청 중:", url);
-                return request(url);
-            case 2:
-                val = v;
-                console.log(val);
-                return;
-            case 3:
-                var err = v;
-                console.log("에러:", err);
-                return false;
-        }
-    }
+// 케이스 2
+var x = [];
+for (var i = 0; i < 10; i++) {
+    x[x.length] = "x";
+}
 
-    // 이터레이터를 만들어 반환
-    return {
-        next: function(v) {
-            // 초기 상태
-            if (!state) {
-                state = 1;
-                return {
-                    done: false,
-                    value: process()
-                };
-            }
-            // yield 재개가 성공
-            else if (state == 1) {
-                state = 2;
-                return {
-                    done: true,
-                    value: process(v)
-                };
-            }
-            // 제너레이터는 이미 완료
-            else {
-                return {
-                    done: true,
-                    value: undefined
-                };
-            }
-        },
-        "throw": function(e) {
-            // 명시적인 에러 처리는 상태 1에만 해당
-            if (state == 1) {
-                state = 3;
-                return {
-                    done: true,
-                    value: process(e)
-                };
-            }
-            // 이 밖의 에러는 처리되지 않으니 그냥 곧바로 던짐
-            else {
-                throw e;
-            }
-        }
-    };
+// 케이스 3
+var x = [];
+for (var i = 0; i < 10; i++) {
+    x.push("x");
 }
