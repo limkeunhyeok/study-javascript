a59deedc2ca25053cebe8ed4f788313084e8b04c
LKH <gorloom6425@naver.com>
2020-09-28 02:30:34 +0900

    you don`t know js 타입과 문법, 스코프와 클로저 정리 (1)

--- study.js
+++ study.js
@@ -1,9 +1,1 @@
-function factorial(n) {
-    function fact(n, res) {
-        if (n < 2) return res;
-        return fact(n - 1, n * res);
-    }
-    return fact(n, 1);
-}
-
-console.log(factorial(100));
+console.log(typeof a);
