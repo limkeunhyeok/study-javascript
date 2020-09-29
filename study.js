937e100bb18d9aa10c5e32f5488213116525cc76
LKH <gorloom6425@naver.com>
2020-09-30 00:01:46 +0900

    you don`t know js this와 객체 프로토타입, 비동기 성능

--- study.js
+++ study.js
@@ -1,11 +1,8 @@
-var a = 2;
-var b = a; // 값-복사
-b++;
-console.log(a); // 2
-console.log(b); // 3
+function foo() {
+    var a = 2;
+    (() => {
+        console.log(a);
+    })();
+}
 
-var c = [1, 2, 3];
-var d = c; // 레퍼런스-복사
-d.push(4);
-console.log(c); // [1, 2, 3, 4]
-console.log(d); // [1, 2, 3, 4]
+foo();
