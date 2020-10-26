28a44966a9fc56df4a355a8a1153958e0f19a907
LKH <gorloom6425@naver.com>
2020-10-26 13:13:43 +0900

    3장 Node.js 이해

--- Node.js 마이크로서비스 코딩 공작소/2부 Node.js로 모놀리식 서비스 만들기/ex/httpClient.js
+++ Node.js 마이크로서비스 코딩 공작소/2부 Node.js로 모놀리식 서비스 만들기/ex/httpClient.js
@@ -1,11 +1,3 @@
-// const http = require("http");
-
-// var server = http.createServer((req, res) => {
-//     res.end("hello world!");
-// });
-
-// server.listen(8000);
-
 var http = require("http");
 
 var options = {
